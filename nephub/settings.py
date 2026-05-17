da2ccd7ba016be9f66e980be0a18690ec87ad592
Rohit Acharya <hitrog69@gmail.com>
2026-05-17 10:47:18 +0545

    Switch to PostgreSQL, fix startup command, add railway.toml

--- nephub/settings.py
+++ nephub/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,10 +72,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'nephub.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', str(BASE_DIR / 'db.sqlite3')),
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
